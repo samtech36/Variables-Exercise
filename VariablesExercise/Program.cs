25a06a11644710e2af9370628ae6e183c5243ba1
samtech36 <samuel.hossain@outlook.com>
2024-08-08 12:20:33 -0400

    Creating my own Variable Exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            String name = "Anthony";
+            int age = 27;
+            char middleInitial = 'J';
+            bool isOver21 = true;
+            double currentTemp = 75.9;
+            decimal currentPrice = 28.99m;
+            
+            
+
+            //String interpolation
+            
+            Console.WriteLine($"My name is {name} and I am {age} years old \n" +
+                              $"My Initial name is {middleInitial}");
+            Console.WriteLine($"I am above the age of 21 which is {isOver21} \n" +
+                              $"The current temperature is {currentTemp} and current price for silver is {currentPrice} dollars");
         }
     }
+    
 }
